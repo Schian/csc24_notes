38ad766b526aa4f9b1c70753c54a6efeee248b35
Adam Wicks <68495803+Schian@users.noreply.github.com>
2024-11-15 12:39:22 +1100

    Added a loop and retrieved flag.

--- traffic_light_protocol.py
+++ traffic_light_protocol.py
@@ -14,57 +14,65 @@ IP: 10.1.253.46 (changes each time the lab resets)
 """
 
 import requests
+import time
 from bs4 import BeautifulSoup
 
 
-##############################################################################
-# Fetch the HTML contents
+
 url = "http://10.1.253.46"  # This will have to be changed on lab reset
-url_response = requests.get(url)
 
-# Check the response code
-if url_response.status_code != 200:
-    print(f"Failed to fetch the page, status code: "
-          f"{url_response.status_code}")
-    exit()
+while True:
+    ##########################################################################
+    # Fetch the HTML contents
+    url_response = requests.get(url)
 
-html_content = url_response.text
+    # Check the response code
+    if url_response.status_code != 200:
+        print(f"Failed to fetch the page, status code: "
+            f"{url_response.status_code}")
+        exit()
 
-##############################################################################
-# Parse the HTML contents
-soup = BeautifulSoup(html_content, 'html.parser')
+    html_content = url_response.text
 
-# Extract the traffic data
-traffic_data = {
-    "carsBusesNorthSouth": int(soup.find(
-        string="Cars and buses, north and south side: ").find_next('b').text),
-    "carsBusesEastWest": int(soup.find(
-        string="Cars and buses, east and west side: ").find_next("b").text),
-    "trains": int(soup.find(
-        string="Trains: ").find_next("b").text),
-    "pedestriansNorthSouth": int(soup.find(
-        string="Pedestrians, north and south side: ").find_next("b").text),
-    "pedestriansEastWest": int(soup.find(
-        string="Pedestrians, east and west side: ").find_next("b").text)
-}
-##############################################################################
-# Determine the next green
-next_green = max(traffic_data, key=traffic_data.get)
+    ##########################################################################
+    # Parse the HTML contents
+    soup = BeautifulSoup(html_content, 'html.parser')
 
-##############################################################################
-# Send the POST request
+    # Extract the traffic data
+    traffic_data = {
+        "carsBusesNorthSouth": int(soup.find(
+            string="Cars and buses, north and south side: ").find_next('b').text),
+        "carsBusesEastWest": int(soup.find(
+            string="Cars and buses, east and west side: ").find_next("b").text),
+        "trains": int(soup.find(
+            string="Trains: ").find_next("b").text),
+        "pedestriansNorthSouth": int(soup.find(
+            string="Pedestrians, north and south side: ").find_next("b").text),
+        "pedestriansEastWest": int(soup.find(
+            string="Pedestrians, east and west side: ").find_next("b").text)
+    }
+    ##########################################################################
+    # Determine the next green
+    next_green = max(traffic_data, key=traffic_data.get)
 
-# Craft the POST request
-post_url = f"{url}/{next_green}"
+    ##########################################################################
+    # Send the POST request
 
-# Send the POST request
-post_response = requests.post(post_url)
+    # Craft the POST request
+    post_url = f"{url}/{next_green}"
 
-# Review the POST response
-if post_response.status_code == 200:
-    print(f"Successfully changed green light to {next_green}.\n"
-          f"\tResponse from the server: {post_response.text}\n")
-else:
-    print(f"Failed to change the green light.\n"
-          f"\tStatus code: {post_response.status_code}"
-          f"\tServer Response: {post_response.text}")
+    # Send the POST request
+    post_response = requests.post(post_url)
+
+    # Review the POST response
+    if post_response.status_code == 200:
+        print(f"Successfully changed green light to {next_green}.\n"
+            f"\tResponse from the server: {post_response.text}\n")
+    else:
+        print(f"Failed to change the green light.\n"
+            f"\tStatus code: {post_response.status_code}"
+            f"\tServer Response: {post_response.text}")
+
+    time.sleep(3)
+
+# flag{8f9asdjk2jd9afjlz}
